e5664925f9d2ef365f0d79c6f7a204da4e7f890f
Raffaele Sabatini <f4ff0@icloud.com>
2026-06-11 17:52:44 +0200

    Splitting train and test datasets

--- models/DopplerDataset.py
+++ models/DopplerDataset.py
@@ -5,36 +5,27 @@ from os import listdir
 from os.path import isdir
 
 class DopplerDataset(Dataset):
-    def __init__(self, dataset_dir, type="train", transform=None, target_transform=None):
+    def __init__(self, dataset_dir, transform=None, target_transform=None):
         self.dataset_dir      = dataset_dir
         self.list_subdir      = [dir_name for dir_name in listdir(self.dataset_dir) if isdir(self.dataset_dir+'/'+dir_name)]
         self.transform        = transform
         self.target_transform = target_transform
 
-    def __len__(self):
-        num_images = 0
-        for dir_name in self.list_subdir:
-            num_images += len(listdir(self.dataset_dir+'/'+dir_name))
-        
-        return num_images
+    def __len__(self):        
+        return len(listdir(self.dataset_dir))
     
-    def __getitem__(self, idx): # <----  Very slow (?)
+    def __getitem__(self, idx):
         sample = []; label = ""
 
-        current_img_num = 0 
-        for dir_name in self.list_subdir:
-            current_img_num += len(listdir(self.dataset_dir+'/'+dir_name))
-            if idx < current_img_num:
-                img_name = sorted(listdir(self.dataset_dir+'/'+dir_name))[idx-current_img_num]
+        img_name = sorted(listdir(self.dataset_dir))[idx]
 
-                sample = np.load(self.dataset_dir+'/'+dir_name+'/'+img_name, allow_pickle=True).T
-                if self.transform:
-                    image = self.transform(image)
+        sample = np.load(self.dataset_dir+'/'+img_name, allow_pickle=True).T
+        if self.transform:
+            image = self.transform(image)
 
-                label  = img_name.split("_")[1]
-                label  = label[0]                   # Labels like J1, J2 are intended as J 
-                if self.target_transform:
-                    label = self.target_transform(label)
-                break
-        
+        label  = img_name.split("_")[1]
+        label  = label[0]                   # Labels like J1, J2 are intended as J 
+        if self.target_transform:
+            label = self.target_transform(label)
+
         return sample, label
